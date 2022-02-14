9a8558beb7ffdde00cae685988e8c9ab85c8933e
ningeen <ningenkun@gmail.com>
2022-02-14 17:41:04 +0300

    brightness check

--- cv_validator/checks/data/__init__.py
+++ cv_validator/checks/data/__init__.py
@@ -1,2 +1,3 @@
+from .brightness_check import BrightnessCheck
 from .color_shift import ColorShift
 from .image_size import ImageSize
---
+++ cv_validator/checks/data/brightness_check.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from ...core.condition import BaseCondition
+from .image_size import ImageSize
+
+
+class BrightnessCheck(ImageSize):
+    def __init__(
+        self,
+        difference_metric: str = "psi",
+        condition: BaseCondition = None,
+        threshold_bright: int = 15,
+        threshold_dark: int = 240,
+    ):
+        super().__init__(difference_metric, condition)
+        self.threshold_bright = threshold_bright
+        self.threshold_dark = threshold_dark
+        self.desired_params = ["bright_ratio", "dark_ratio"]
+
+    def get_name(self) -> str:
+        return "Image brightness check."
+
+    def get_description(self) -> str:
+        return "Compares image brightness distributions."
+
+    def calc_img_params(self, img: np.array) -> dict:
+        result = dict()
+        is_grey = len(img.shape) == 2 or img.shape[2] == 1
+
+        if not is_grey:
+            img = img.mean(axis=-1)
+
+        result["bright_ratio"] = np.mean(img < self.threshold_bright)
+        result["dark_ratio"] = np.mean(img >= self.threshold_dark)
+        return result
